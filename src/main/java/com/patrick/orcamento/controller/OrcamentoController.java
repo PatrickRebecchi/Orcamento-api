c216c4d697117451451ab7de34118fc6a1695853
Patrick <patrickrebecchi@hotmail.com>
2026-03-11 20:46:06 -0300

    feat: implementa cadastro de orçamentos

--- src/main/java/com/patrick/orcamento/controller/OrcamentoController.java
+++ src/main/java/com/patrick/orcamento/controller/OrcamentoController.java
@@ -1,10 +1,17 @@
 package com.patrick.orcamento.controller;
 
 
+import com.patrick.orcamento.dto.OrcamentoDTO;
+import com.patrick.orcamento.dto.VeiculoDTO;
+import com.patrick.orcamento.exception.OrcamentoException;
 import com.patrick.orcamento.service.OrcamentoService;
+import jakarta.validation.Valid;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/orcamento")
@@ -12,4 +19,27 @@ public class OrcamentoController {
 
     @Autowired
     private OrcamentoService service;
+
+    @GetMapping
+    public List<OrcamentoDTO> orçamentos(){
+        return service.listar();
+    }
+
+    @PostMapping
+    public OrcamentoDTO cadastrar(@Valid @RequestBody OrcamentoDTO dto){
+        return service.cadastrar(dto);
+    }
+
+    @PatchMapping("/{id}/aprovar")
+    public ResponseEntity<String> aprovar(@PathVariable Long id){
+        service.aprovar(id);
+        return ResponseEntity.ok("Orçamento aprovado com sucesso");
+    }
+
+    @PatchMapping("/{id}/finalizado")
+    public ResponseEntity<String> finalizar(@PathVariable Long id){
+        service.finalizar(id);
+        //return ResponseEntity.ok("Orçamento finalizado");
+        return ResponseEntity.ok("Orçamento finalizado com sucesso");
+    }
 }
--- src/main/java/com/patrick/orcamento/service/OrcamentoService.java
+++ src/main/java/com/patrick/orcamento/service/OrcamentoService.java
@@ -1,8 +1,22 @@
 package com.patrick.orcamento.service;
 
+import com.patrick.orcamento.dto.ClienteDTO;
+import com.patrick.orcamento.dto.OrcamentoDTO;
+import com.patrick.orcamento.dto.VeiculoClienteDTO;
+import com.patrick.orcamento.dto.VeiculoDTO;
+import com.patrick.orcamento.entity.Orcamento;
+import com.patrick.orcamento.entity.Status;
+import com.patrick.orcamento.entity.Veiculo;
+import com.patrick.orcamento.exception.OrcamentoException;
 import com.patrick.orcamento.repository.OrcamentoRepository;
+import com.patrick.orcamento.repository.VeiculoRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OrcamentoService {
@@ -10,4 +24,50 @@ public class OrcamentoService {
     @Autowired
     private OrcamentoRepository repository;
 
+    @Autowired
+    private VeiculoRepository veiculoRepository;
+
+    public List<OrcamentoDTO> listar() {
+        return converteDados(repository.findAll());
+    }
+
+    @Transactional
+    public OrcamentoDTO cadastrar(OrcamentoDTO dto) {
+
+        Veiculo veiculo = veiculoRepository.findById(dto.veiculoId())
+                        .orElseThrow(() -> new OrcamentoException("Veiculo não encontrado"));
+
+        Orcamento o = new Orcamento();
+        o.setData(dto.data());
+        o.setStatus(Status.ABERTO);
+        o.setVeiculo(veiculo);
+
+        repository.save(o);
+
+        return new OrcamentoDTO(
+                o.getData(),
+                o.getStatus().name(),
+                o.getVeiculo().getId());
+    }
+
+    private List<OrcamentoDTO> converteDados(List<Orcamento> orcamentos){
+        return orcamentos.stream()
+                .map(o -> new OrcamentoDTO(o.getData(),
+                        o.getStatus().name(),
+                        o.getVeiculo().getId()))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void aprovar(Long id){
+        Orcamento orcamento = repository.findById(id)
+                .orElseThrow(() -> new OrcamentoException("Orçamento não encontrado"));
+        orcamento.setStatus(Status.APROVADO);
+    }
+
+    public void finalizar(Long id) {
+        Orcamento orcamento = repository.findById(id)
+                .orElseThrow(()-> new OrcamentoException("Orçamento não encontrado"));
+        orcamento.setStatus(Status.FINALIZADO);
+    }
 }
